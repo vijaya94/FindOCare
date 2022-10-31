3a6274512e5df6bbff4bf8fa42d2b5b62bec6398
vijaya94 <bhartiv@northeastern.edu>
2022-10-31 12:40:34 -0400

    Adding Patient registration form

---
+++ src/model/Doctor.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author HP
+ */
+public class Doctor {
+    private String name;
+    private int contact;
+    private String degree;
+    private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+}
---
+++ src/model/PatientHistory.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sajja
+ */
+public class PatientHistory {
+    private ArrayList<Patient> historyP;
+    
+    public PatientHistory(){
+        
+    this.historyP = new ArrayList<Patient>();
+    }
+
+    public ArrayList<Patient> getHistoryP() {
+        return historyP;
+    }
+
+    public void setHistoryP(ArrayList<Patient> historyP) {
+        this.historyP = historyP;
+    }
+    
+    public Patient addNewPatient(){
+        
+        Patient newPatient = new Patient();
+        
+        historyP.add(newPatient);
+        return newPatient;
+     }
+}
